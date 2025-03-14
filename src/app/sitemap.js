1b50559863876710463803e899d3a2815471933c
SOLGOR1 <info.sorgol@proton.me>
2025-03-14 22:04:05 +0100

    Updated sitemap with all pages

---
+++ src/app/sitemap.js
@@ -0,0 +1,40 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://www.solanaholderbot.com/',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 1.0,
+      },
+      {
+        url: 'https://www.solanaholderbot.com/guide',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://www.solanaholderbot.com/benefits',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://www.solanaholderbot.com/how-it-works',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://www.solanaholderbot.com/testimonials',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.7,
+      },
+      {
+        url: 'https://www.solanaholderbot.com/faqs',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.7,
+      },
+    ];
+  }
