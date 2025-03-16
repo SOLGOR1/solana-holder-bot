64276850256c292a0f88f81e37f62c9bf8ef8c04
SOLGOR1 <info.sorgol@proton.me>
2025-03-16 13:55:14 +0100

    Optimized Sitemap  for SEO

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -6,35 +6,5 @@ export default function sitemap() {
         changeFrequency: 'weekly',
         priority: 1.0,
       },
-      {
-        url: 'https://www.solanaholderbot.com/guide',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.8,
-      },
-      {
-        url: 'https://www.solanaholderbot.com/benefits',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.8,
-      },
-      {
-        url: 'https://www.solanaholderbot.com/how-it-works',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.8,
-      },
-      {
-        url: 'https://www.solanaholderbot.com/testimonials',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.7,
-      },
-      {
-        url: 'https://www.solanaholderbot.com/faqs',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.7,
-      },
     ];
   }
