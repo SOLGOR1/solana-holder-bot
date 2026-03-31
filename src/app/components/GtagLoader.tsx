2e638209e083cd544076bd5257594bba6c314684
SOLGOR1 <info.sorgol@proton.me>
2026-03-31 18:51:18 +0200

    Update

--- src/app/components/GtagLoader.tsx
+++ src/app/components/GtagLoader.tsx
@@ -1,36 +1,26 @@
 // src/app/components/GtagLoader.tsx
-"use client";
-
-import { useEffect } from "react";
-
-declare global {
-  interface Window {
-    dataLayer: any[];
-    gtag: (...args: any[]) => void;
-  }
-}
+import Script from "next/script";
 
 export default function GtagLoader() {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      // GTAG Script hinzufügen
-      const script = document.createElement("script");
-      script.src = "https://www.googletagmanager.com/gtag/js?id=AW-18035540031";
-      script.async = true;
-      document.head.appendChild(script);
-
-      // dataLayer initialisieren
-      window.dataLayer = window.dataLayer || [];
-      window.gtag = function (...args: any[]) {
-        window.dataLayer.push(args);
-      };
-
-      window.gtag("js", new Date());
-      window.gtag("config", "AW-18035540031");
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  return null;
+  return (
+    <>
+      {/* GTAG Script laden */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-18035540031"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="google-ads"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-18035540031');
+          `,
+        }}
+      />
+    </>
+  );
 }
